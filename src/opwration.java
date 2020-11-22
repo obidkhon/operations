7fe179515ff6549828c9dc7b72b5e35eda70fa3a
obidkon <obid.sharipov.80@mail.ru>
2020-11-22 19:49:36 +0500

    operat

---
+++ src/opwration.java
@@ -0,0 +1,29 @@
+public class opwration {
+    public static void main(String[] args) {
+        //== teng bir-biridan
+        // <= kinchik yoki teng bir-biriga
+        // < kichik bir-biridsn
+        // >= katta yoki teng  bir-biridsn
+        // > katta bir-biridan
+        //! bu unay emas belgisi (inkor)
+        //  bu || yoki belgisi
+        // bu && ikita haqiaatni ishlat[sh uchun
+
+
+        double a = 4.6;
+        double b = 3.4;
+        if (a == 4.6) {
+
+            System.out.println("Teng bo'ladi");
+        }
+        if (a > 6) {
+
+        }
+        if (a==4.6||a==4.9){
+            System.out.println("manabuxam bor");
+        }
+        if (a>0&&a<10){
+            System.out.println("Mana bo'lasa");
+        }
+    }
+}
